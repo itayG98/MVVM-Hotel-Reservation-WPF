4fd9de225ed6b987abb1e988ba7f4490227a8a64
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-09-09 10:22:23 +0300

    To short Data string fromating in LVReservation View

--- MVVM/ViewModel/ReservationViewModel.cs
+++ MVVM/ViewModel/ReservationViewModel.cs
@@ -12,8 +12,8 @@ namespace MVVM.ViewModel
         private readonly Reservation reservation;
         public string RoomID => reservation.roomID?.ToString();
         public string UserName => reservation.UserName;
-        public DateTime StartDate => reservation.Start;
-        public DateTime EndDate => reservation.End; 
+        public string StartDate => reservation.Start.ToString("d");
+        public string EndDate => reservation.End.ToString("d");
         public TimeSpan Duration => reservation.Duration;
 
         public ReservationViewModel(Reservation resev)
