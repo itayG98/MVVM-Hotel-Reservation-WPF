db0264daa642dd440af1c7dfead3c5f6de949892
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-09-09 09:12:38 +0300

    Binded Iobservalbe ViewModel of Reservation

--- MVVM/MainWindow.xaml.cs
+++ MVVM/MainWindow.xaml.cs
@@ -31,20 +31,6 @@ namespace MVVM
         public MainWindow()
         {
             InitializeComponent();
-            try
-            {
-                hotel.MakeReserbation(new Reservation("Itay", new RoomID(1, 1), new DateTime(2022, 1, 1), new DateTime(2022, 1, 10)));
-                hotel.MakeReserbation(new Reservation("Itay", new RoomID(1, 1), new DateTime(2022, 2, 1), new DateTime(2022, 2, 10)));
-                hotel.MakeReserbation(new Reservation("Shon", new RoomID(2, 1), new DateTime(2022, 2, 1), new DateTime(2022, 2, 10)));
-                hotel.MakeReserbation(new Reservation("Shon", new RoomID(2, 2), new DateTime(2022, 2, 12), new DateTime(2022, 2, 14)));
-                hotel.MakeReserbation(new Reservation("Shon", new RoomID(2, 2), new DateTime(2022, 1, 1), new DateTime(2022, 1, 7)));
-            }
-            catch (ReservationConflictException ex)
-            {
-                MessageBox.Show($"{ex.ExistingReservation} Conflicts with {ex.IncomingReservation} ", "Error");
-            }
-            IEnumerable<Reservation> reservations = hotel.GetReservations();
-
         }
     }
 }
--- MVVM/ViewModel/ReservationLVViewModel.cs
+++ MVVM/ViewModel/ReservationLVViewModel.cs
@@ -12,11 +12,19 @@ namespace MVVM.ViewModel
     public class ReservationLVViewModel : ViewModelBase
     {
         private readonly ObservableCollection<ReservationViewModel> _reservations;
+
+        public IEnumerable<ReservationViewModel> Reservations => _reservations;
         public ICommand MakeReservation { get; }
 
         public ReservationLVViewModel()
         {
             _reservations = new ObservableCollection<ReservationViewModel>();
+            _reservations.Add(new ReservationViewModel(new Reservation("Itay", new RoomID(1, 1), new DateTime(2022, 1, 1), new DateTime(2022, 1, 10))));
+            _reservations.Add(new ReservationViewModel(new Reservation("Itay", new RoomID(1, 1), new DateTime(2022, 2, 1), new DateTime(2022, 2, 10))));
+            _reservations.Add(new ReservationViewModel(new Reservation("Shon", new RoomID(2, 1), new DateTime(2022, 2, 1), new DateTime(2022, 2, 10))));
+            _reservations.Add(new ReservationViewModel(new Reservation("Shon", new RoomID(2, 2), new DateTime(2022, 2, 12), new DateTime(2022, 2, 14))));
+            _reservations.Add(new ReservationViewModel(new Reservation("Shon", new RoomID(2, 2), new DateTime(2022, 1, 1), new DateTime(2022, 1, 7))));
+
         }
     }
 }
